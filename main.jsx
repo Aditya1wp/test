23d4f390122c981c31a5c0dabce8ea171ea32e10
Aditya1wp <adityagaurav1122@gmail.com>
2026-04-13 19:03:31 +0530

    Final stability fix: add jsconfig.json and rename to Root.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './NIMCETApp'
+import App from './Root'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
